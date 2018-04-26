c51d435a6baf4452532cc147c2d0244b7493bc1f
changeyu <coder.zy@qq.com>
2018-04-26 18:54:13 +0800

    222

--- src/components/quill/hiddenBlock/index.js
+++ src/components/quill/hiddenBlock/index.js
@@ -3,7 +3,7 @@ let Block = Quill.import('formats/code-block');
 
 
 class Blockquote extends Block { }
-Blockquote.blotName = 'hidden-block1';
+Blockquote.blotName = 'hidden-block2';
 // Blockquote.tagName = 'CODE';
 // Blockquote.className = 'hidden-block';
 
